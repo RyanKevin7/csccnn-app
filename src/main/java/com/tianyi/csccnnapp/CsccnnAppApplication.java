301a36b58cf01ed555ffb22dca8169ee0311846a
zhengkai5 <zhengkai5@asiainfo.com>
2025-02-17 09:36:49 +0800

    Initial commit

---
+++ src/main/java/com/tianyi/csccnnapp/CsccnnAppApplication.java
@@ -0,0 +1,13 @@
+package com.tianyi.csccnnapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsccnnAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsccnnAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tianyi/csccnnapp/CsccnnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tianyi.csccnnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsccnnAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
